baab7d4f870322ef96fabdd9cdcc7044a41c81dd
Manu Vilachán <mvilachan@gmail.com>
2022-06-07 11:09:24 +0200

    uptade scripts with last entities (#22)
    Co-authored-by: patrihr <patri_hr21@hotmail.com>

---
+++ sample/NormaSample.Db/Scripts/PriorityGroups.sql
@@ -0,0 +1,10 @@
+﻿MERGE INTO [norma].[PriorityGroups] AS Target
+USING ( VALUES 
+	('Priority Group 1', 1),
+    ('Priority Group 2', 2)
+)
+AS Source ([Name], [Priority])
+ON Target.[Name] = Source.[Name]
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT ([Name], [Priority])
+	VALUES ([Name], [Priority]);
