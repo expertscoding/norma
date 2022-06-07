f6ed4282619e4a085d285e2a7723f9dd284bbb2a
Manu Vilachán <mvilachan@gmail.com>
2022-06-07 11:16:12 +0200

    Change name fields in table from varchar(20) to nvarchar (1024) (#23)
    Co-authored-by: patrihr <patri_hr21@hotmail.com>
    Previously approved and merged into main

--- src/EC.Norma.Db/Tables/PriorityGroups.sql
+++ src/EC.Norma.Db/Tables/PriorityGroups.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [norma].[PriorityGroups]
 (
 	[Id] INT NOT NULL IDENTITY, 
-	[Name] VARCHAR(20) NOT NULL,
+	[Name] NVARCHAR(1024) NOT NULL,
 	[Priority] INT NOT NULL,
 
     CONSTRAINT [PK_PriorityGroups] PRIMARY KEY ([Id]) 
